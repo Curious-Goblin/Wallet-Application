07b1b90033432769a728744c0e1673dd0cdfece5
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-09-24 19:35:34 +0530

    wallet application added

---
+++ apps/user-app/app/lib/auth.ts
@@ -0,0 +1,80 @@
+import db from "@repo/db/client";
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt";
+import { JWT } from "next-auth/jwt";
+import { Session } from "inspector";
+
+interface Credentials {
+    phone: string;
+    password: string;
+}
+
+interface ExtendedSession extends Session {
+    user: {
+        id: string;
+        name: string;
+        email: string;
+    };
+}
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
+                password: { label: "Password", type: "password", required: true }
+            },
+            async authorize(credentials: Credentials | undefined) {
+                if (!credentials) {
+                    return null;
+                }
+                // Do zod validation, OTP validation here
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
+                    }
+                });
+
+                if (existingUser) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        }
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        email: user.number
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+
+                return null
+            },
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        async session({ token, session }: { token: JWT, session: ExtendedSession }) {
+            session.user.id = token.sub ?? "token";
+            return session
+        }
+    }
+}
